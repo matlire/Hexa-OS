22da1fd4eb5f03a0a71a307f3fb0cb507c730464
matlire <mr.matlire@gmail.com>
2025-06-09 16:19:32 +0300

    Small test changes

--- src/x86_64/kernel/main.c
+++ src/x86_64/kernel/main.c
@@ -3,5 +3,5 @@
 void kernel_main() {
     terminal_clear();
     terminal_set_color(COLOR_YELLOW, COLOR_BLACK);
-    terminal_write_str("> Welcome to Hexa OS!");
+    terminal_write_str("> Welcome to Hexa OS!\n");
 }
